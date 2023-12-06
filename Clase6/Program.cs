4e16a0191173bbe932d3f60637692a97ed63ab44
Joaquin Ignacio Correa <joaquincorrea006@gmail.com>
2023-12-06 02:31:51 -0300

    Se realiza escritura y lectura de datos

--- Clase6/Program.cs
+++ Clase6/Program.cs
@@ -8,103 +8,59 @@ namespace Clase6
     {
         static void Main(string[] args)
         {
-
-            //string path = @"C:\Users\aleja\Desktop\Curso NET CDA\prueba.txt";
-            //string message = "\n segunda linea";
-            //File.AppendAllText(path, message);
-            //string path = @"C:\Users\aleja\Desktop\Curso NET CDA\prueba.txt";
-
-
-            //string path = @"C:\Users\aleja\Desktop\Curso NET CDA\data.txt";
-
-            //string[] data = File.ReadAllLines(path);
-
-            //var lstUsuarios = new List<UsuariosDTO>();
-
-            //foreach (string line in data) 
-            //{
-            //    var newData = line.Split(";");
-
-            //    var newUsuario = new UsuariosDTO() 
-            //    {
-            //       Codigo = newData[0].ToUpper().Trim(),
-            //       Nombre = newData[1].ToLower().Trim(),
-            //       Apellido = newData[2].ToLower().Trim(),
-            //    };
-            //    lstUsuarios.Add(newUsuario);
-            //}
-
-            string connectionString = @"Data Source=localhost\SQL2022;Initial Catalog=Pruebas;Integrated Security=True;TrustServerCertificate=True";
-            SqlConnection connection = new SqlConnection(connectionString);
-
-            //...
-            #region Consulta de datos
-            //connection.Open();
-            //SqlCommand command = connection.CreateCommand();
-
-            //command.CommandType = System.Data.CommandType.Text;
-            //command.CommandText = @"select *  from Usuario";
-
-
-            //SqlDataReader reader = command.ExecuteReader();
-            //var lstUsuarios = new List<UsuariosDTO>();
-            //while (reader.Read()) 
-            //{
-            //    var id = int.Parse( reader["id"].ToString()!  ); 
-            //    var nombre = reader["nombre"].ToString()!;
-            //    var apellido = reader["apellido"].ToString()!;
-
-            //    var newUsuario = new UsuariosDTO()
-            //    {
-            //        Codigo = id,
-            //        Nombre = nombre,
-            //        Apellido = apellido
-            //    };
-            //    lstUsuarios.Add(newUsuario);
-            //}
-
-            //connection.Close();
-
-
-            //foreach ( var user in lstUsuarios ) 
-            //{
-            //    Console.WriteLine(JsonSerializer.Serialize(user));
-            //}
-            #endregion
-
-            connection.Open();
-
-            SqlCommand cmd = connection.CreateCommand();
-
-            string nombre = "Joaquin";
-            string apellido = "Correa') truncate table usuario --";
-            //cmd.CommandText = $"insert into Usuario(nombre,apellido) values('{nombre}','{apellido}')";
-
+            //Pasaje de Archivo de texto a una lista de objetos
+            string DireccionArchivo = @"C:\Users\taxrem\OneDrive\Escritorio\Git Prueba\file-ado\data.txt";
+            List<UsuarioPracticaJC> ListaUsuariosEscritura = new List<UsuarioPracticaJC>();
+            string[] ListaUsuarios = File.ReadAllLines(DireccionArchivo);
             
-            SqlParameter parametroNombre = new SqlParameter() 
+            foreach(var Usuario in ListaUsuarios)
             {
-                ParameterName = "nombre",
-                SqlDbType = System.Data.SqlDbType.VarChar,
-                Value = nombre
+                string[] DatosUsuarios = Usuario.Split(';');
+                UsuarioPracticaJC NuevoUsuario = new UsuarioPracticaJC(DatosUsuarios[0].ToLower().Trim(), DatosUsuarios[1].ToLower().Trim(), DatosUsuarios[2].ToLower().Trim());
+                ListaUsuariosEscritura.Add(NuevoUsuario);
+            }
 
-            };
-            SqlParameter parametroApellido = new SqlParameter()
+            //Conexion de SqlMS
+            string ConexionBddString = @"Data Source=localhost;Initial Catalog=PruebasCapacitacion;Integrated Security=True;Trust Server Certificate=True";
+            SqlConnection ConexionBdd = new SqlConnection(ConexionBddString);
+
+            ConexionBdd.Open();
+            //Se escribe lista de usuarios en base de datos SQL
+            SqlCommand UsuariosBaseDatos = ConexionBdd.CreateCommand();
+            foreach (var Usuario in ListaUsuariosEscritura)
             {
-                ParameterName = "apellido",
-                SqlDbType = System.Data.SqlDbType.VarChar,
-                Value = apellido
+                try
+                {
+                    UsuariosBaseDatos.CommandText = $"insert into Usuario(id,nombre,apellido) values('{Usuario.IdUsuario}','{Usuario.Nombre}','{Usuario.Apellido}')";
+                    int Columna = UsuariosBaseDatos.ExecuteNonQuery();
+                    if (Columna > 0) Console.WriteLine($"Se agrego usuario {Usuario.Nombre} con exito");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al insertar los usuarios.");
+                }
+            }
 
-            };
+            //Se lee base de datos Sql y pasamos datos a una lista de objetos
+            SqlCommand UsuariosBaseDatosLec = ConexionBdd.CreateCommand();
+            UsuariosBaseDatosLec.CommandText = @"select *  from Usuario";
+            using (SqlDataReader LectorUsuarios = UsuariosBaseDatosLec.ExecuteReader())
+            {
+                List<UsuarioPracticaJC> ListaUsuariosLectura = new List<UsuarioPracticaJC>();
 
-            cmd.CommandText = $"insert into Usuario(nombre,apellido) values(@nombre,@apellido)";
-            cmd.Parameters.Add(parametroNombre);
-            cmd.Parameters.Add(parametroApellido);
-          
-            int rows =  cmd.ExecuteNonQuery();
-            if(rows > 0) Console.WriteLine("Se agrego correctamente...");
+                while (LectorUsuarios.Read())
+                {
+                    UsuarioPracticaJC NuevoUsuario = new UsuarioPracticaJC(LectorUsuarios["id"].ToString(), LectorUsuarios["nombre"].ToString(), LectorUsuarios["apellido"].ToString());
+                    ListaUsuariosLectura.Add(NuevoUsuario);
+                    Console.WriteLine($"Usuario: {NuevoUsuario.IdUsuario} {NuevoUsuario.Nombre} {NuevoUsuario.Apellido}");
+                }
+            }
 
-            connection.Close();
+            UsuariosBaseDatos.CommandText = $"truncate table usuario";
+            int ColumnaT = UsuariosBaseDatos.ExecuteNonQuery();
+            if (ColumnaT == -1) Console.WriteLine($"Se reseteo tabla con exito");
 
+            ConexionBdd.Close();
         }
     }
 }
---
+++ Clase6/UsuarioPracticaJC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase6
+{
+    internal class UsuarioPracticaJC
+    {
+        public UsuarioPracticaJC(string? idUsuario, string nombre, string apellido)
+        {
+            IdUsuario = idUsuario;
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+        public string? IdUsuario { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+    }
+}
